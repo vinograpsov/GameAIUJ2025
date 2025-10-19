d42b520633d9c3b8af8fc38e92c34b6df4584336
Kiryl Vinahradau <kirvinpost@gmail.com>
2025-10-19 15:01:30 +0200

    make field and obstacles

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -8,7 +8,7 @@ from obstacle import Obstacle
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
-screen = pygame.displayset_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Zombie Survival Killer Mega Bestseller 3000")
 
 clock = pygame.time.Clock()
@@ -23,5 +23,22 @@ for _ in range(10):
     obstacles.append(Obstacle(x, y, radius, color))
 
 
+running = True
+while running:
+    clock.tick(FPS)
+    
+    for event in pygame.event.get():
+        if  event.type == pygame.QUIT:
+            running = False
 
+    
+    screen.fill((50, 50, 50))
+    pygame.draw.rect(screen, (0,0,0), (0,0, WIDTH, HEIGHT), 10)
 
+    for o in obstacles:
+        o.draw(screen)
+
+    pygame.display.flip()
+
+pygame.quit()
+sys.exit()
