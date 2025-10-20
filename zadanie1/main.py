fdfb6fda053b4dc1c22bb59e68c653f41ed05d4e
Kiryl Vinahradau <kirvinpost@gmail.com>
2025-10-20 19:15:38 +0200

    add enemies and their collision

--- zadanie1/main.py
+++ zadanie1/main.py
@@ -4,6 +4,7 @@ import sys
 
 from obstacle import Obstacle
 from player import Player
+from enemy import Enemy
 
 pygame.init()
 
@@ -25,6 +26,14 @@ for _ in range(10):
 
 player = Player(WIDTH // 2, HEIGHT // 2, debug=True)
 
+
+enemies = []
+for _ in range(5):
+    x = random.randint(50, WIDTH - 50)
+    y = random.randint(50, HEIGHT - 50)
+    enemies.append(Enemy(x, y))
+
+
 running = True
 while running:
     clock.tick(FPS)
@@ -42,12 +51,21 @@ while running:
         player.resolve_collision(o)
             
 
+    for e in enemies:
+        e.update(player)
+         
+        for o in obstacles:
+            e.resolve_collision(o)
 
     screen.fill((50, 50, 50))
     pygame.draw.rect(screen, (0,0,0), (0,0, WIDTH, HEIGHT), 10)
 
     for o in obstacles:
         o.draw(screen)
+
+    for e in enemies:
+        e.draw(screen)
+
     player.draw(screen)
 
 
